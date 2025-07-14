807eed6ffb6cf311440e17a44688fe7946d52404
Adam Chalmers <adam.chalmers@zoo.dev>
2025-07-14 16:19:39 -0500

    Add formatter + CI check (#9)

--- grammar.js
+++ grammar.js
@@ -2,255 +2,235 @@
 // @ts-check
 
 const PREC = {
-  mul: 12,
-  div: 12,
-  mod: 12,
-  add: 11,
-  sub: 11,
-  pow: 13,
-  cmp: 9,
-  eq: 8,
-  neq: 8,
-  and: 7,
-  or: 6,
-  prefix: 20,
-  pipe: 5, // |>
+	mul: 12,
+	div: 12,
+	mod: 12,
+	add: 11,
+	sub: 11,
+	pow: 13,
+	cmp: 9,
+	eq: 8,
+	neq: 8,
+	and: 7,
+	or: 6,
+	prefix: 20,
+	pipe: 5, // |>
 };
 
 module.exports = grammar({
-  name: 'kcl',
+	name: "kcl",
 
-  rules: {
+	rules: {
+		kcl_program: ($) =>
+			seq(
+				optional(field("shebang", $.shebang)),
+				repeat(seq(optional($.annotation), $.body_item)),
+			),
 
-    kcl_program: $ => seq(
-      optional(field("shebang", $.shebang)),
-      repeat(seq(optional($.annotation), $.body_item)),
-    ),
+		body_item: ($) =>
+			choice(
+				$.expr_stmt,
+				$.variable_declaration,
+				$.return_stmt,
+				$.comment,
+				$.import_stmt,
+			),
 
-    body_item: $ => choice(
-      $.expr_stmt,
-      $.variable_declaration,
-      $.return_stmt,
-      $.comment,
-      $.import_stmt,
-    ),
+		shebang: (_) => /#![^\n]*/,
 
-    shebang: (_) => /#![^\n]*/,
+		import_stmt: ($) =>
+			seq("import", $.string, optional(seq("as", $.identifier))),
 
-    import_stmt: $ => seq("import", $.string, optional(seq("as", $.identifier))),
+		expr_stmt: ($) => $._expr,
 
-    expr_stmt: $ => $._expr,
+		variable_declaration: ($) =>
+			seq(optional("export"), choice($.fn_definition, $.non_fn_definition)),
 
-    variable_declaration: $ => seq(
-      optional('export'),
-      choice(
-        $.fn_definition,
-        $.non_fn_definition,
-      )
-    ),
+		fn_definition: ($) =>
+			seq(
+				"fn",
+				$.identifier,
+				$.param_list,
+				optional(seq(choice(":", "?:"), $.type_name)),
+				"{",
+				repeat($.body_item),
+				"}",
+			),
 
-    fn_definition: $ => seq(
-      'fn',
-      $.identifier,
-      $.param_list,
-      optional(seq(choice(":", "?:"), $.type_name)),
-      '{',
-      repeat($.body_item),
-      '}',
-    ),
+		param_list: ($) => seq("(", commaSep($.param), ")"),
 
-    param_list: $ => seq(
-      '(',
-      commaSep($.param),
-      ')',
-    ),
+		param: ($) =>
+			seq(
+				optional("@"),
+				$.identifier,
+				optional("?"),
+				optional(seq(":", $.type_name)),
+			),
 
-    param: $ => seq(optional("@"), $.identifier, optional("?"), optional(seq(":", $.type_name))),
+		type_name: ($) => seq($._one_type, repeat(seq("|", $._one_type))),
 
-    type_name: $ => seq($._one_type, repeat(seq("|", $._one_type))),
+		_one_type: ($) =>
+			seq($.identifier, optional(seq("(", field("units", $.identifier), ")"))),
 
-    _one_type: $ => seq($.identifier, optional(seq("(", field("units", $.identifier), ")"))),
+		annotation: ($) =>
+			seq("@(", $.annotation_kv, optional(seq(",", $.annotation_kv)), ")"),
 
-    annotation: $ => seq("@(", $.annotation_kv, optional(seq(",", $.annotation_kv)), ")"),
+		annotation_kv: ($) => seq($.identifier, "=", $.identifier),
 
-    annotation_kv: $ => seq($.identifier, "=", $.identifier),
+		identifier: (_) => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
-    identifier: $ =>
-      /[a-zA-Z_][a-zA-Z0-9_]*/,
+		_expr: ($) =>
+			choice(
+				$.number,
+				$.string,
+				$.boolean,
+				$.identifier,
+				$.binary_expr,
+				$.prefix_expr,
+				$.array_expr,
+				$.fn_call,
+				$.pipe_sub,
+			),
 
-    _expr: $ => choice(
-      $.number,
-      $.string,
-      $.boolean,
-      $.identifier,
-      $.binary_expr,
-      $.prefix_expr,
-      $.array_expr,
-      $.fn_call,
-      $.pipe_sub,
-    ),
+		array_expr: ($) => seq("[", optional(commaSep($._expr)), "]"),
 
-    array_expr: $ => seq('[', optional(commaSep($._expr)), ']'),
+		pipe_sub: (_) => "%",
 
-    pipe_sub: $ => '%',
+		fn_call: ($) =>
+			seq(
+				field("callee", $.identifier),
+				"(",
+				commaSep(choice(field("unlabeledArg", $._expr), $.labeledArg)),
+				")",
+			),
 
-    fn_call: $ => seq(
-      field("callee", $.identifier),
-      '(',
-      commaSep(choice(field("unlabeledArg", $._expr), $.labeledArg)),
-      ')'
-    ),
+		labeledArg: ($) =>
+			seq(field("label", $.identifier), "=", field("arg", $._expr)),
 
-    labeledArg: $ => seq(field("label",$.identifier), '=', field("arg", $._expr)),
+		function_body: ($) => seq("{", seq($.body_item), "}"),
 
-    function_body: $ => seq(
-      '{',
-      seq($.body_item),
-      '}',
-    ),
+		return_stmt: ($) => seq("return", $._expr),
 
-    return_stmt: $ => seq(
-      'return',
-      $._expr,
-    ),
+		non_fn_definition: ($) => seq($.identifier, "=", $._expr),
 
-    non_fn_definition: $ => seq(
-      $.identifier,
-      '=',
-      $._expr,
-    ),
+		string: ($) => choice(seq('"', '"'), seq('"', $._string_content, '"')),
 
-    string: $ => choice(
-      seq('"', '"'),
-      seq('"', $._string_content, '"'),
-    ),
+		boolean: (_) => choice("true", "false"),
 
-    boolean: $ => choice("true", "false"),
+		_string_content: ($) =>
+			repeat1(choice($._normal_string_content, $.escape_sequence)),
 
-    _string_content: $ => repeat1(choice(
-      $._normal_string_content,
-      $.escape_sequence,
-    )),
+		_normal_string_content: (_) => token.immediate(prec(1, /[^\\"\n]+/)),
 
-    _normal_string_content: _ => token.immediate(prec(1, /[^\\"\n]+/)),
+		escape_sequence: (_) => token.immediate(seq("\\", /("|\\|\/|b|f|n|r|t|u)/)),
+		prefix_expr: ($) =>
+			prec.right(
+				PREC.prefix,
+				seq(field("operator", $.prefix_operator), field("operand", $._expr)),
+			),
 
-    escape_sequence: _ => token.immediate(seq(
-      '\\',
-      /(\"|\\|\/|b|f|n|r|t|u)/,
-    )),
-    prefix_expr: ($) =>
-      prec.right(
-        PREC.prefix,
-        seq(field("operator", $.prefix_operator), field("operand", $._expr)),
-      ),
+		prefix_operator: (_) => choice("!", "-"),
 
-    prefix_operator: (_) => choice("!", "-"),
+		binary_operator: (_) =>
+			choice(
+				"+",
+				"-",
+				"/",
+				"*",
+				"^",
+				"%",
+				"==",
+				"!=",
+				">",
+				">=",
+				"<",
+				"<=",
+				"|",
+				"&",
+				"|>",
+			),
 
-    binary_operator: (_) =>
-      choice(
-        "+",
-        "-",
-        "/",
-        "*",
-        "^",
-        "%",
-        "==",
-        "!=",
-        ">",
-        ">=",
-        "<",
-        "<=",
-        "|",
-        "&",
-        "|>",
-      ),
+		binary_expr: ($) => {
+			const table = [
+				[prec.right, PREC.pow, "^"],
+				[prec.left, PREC.cmp, choice(">", "<", ">=", "<=")],
+				[prec.left, PREC.eq, "=="],
+				[prec.left, PREC.neq, "!="],
+				[prec.left, PREC.add, choice("+", "-")],
+				[prec.left, PREC.mul, choice("*", "/", "%")],
+				[prec.right, PREC.and, "&"],
+				[prec.left, PREC.or, "|"],
+				[prec.left, PREC.pipe, "|>"],
+			];
 
-    binary_expr: ($) => {
-      const table = [
-        [prec.right, PREC.pow, "^"],
-        [
-          prec.left,
-          PREC.cmp,
-          choice(">", "<", ">=", "<="),
-        ],
-        [prec.left, PREC.eq, "=="],
-        [prec.left, PREC.neq, "!="],
-        [prec.left, PREC.add, choice("+", "-",)],
-        [prec.left, PREC.mul, choice("*", "/", "%")],
-        [prec.right, PREC.and, "&"],
-        [prec.left, PREC.or, "|"],
-        [prec.left, PREC.pipe, "|>"],
-      ];
+			return choice(
+				...table.map(([fn, prec, op]) =>
+					//@ts-ignore
+					fn(
+						prec,
+						seq(
+							field("lhs", $._expr),
+							//@ts-ignore
+							field("operator", alias(op, $.binary_operator)),
+							field("rhs", $._expr),
+						),
+					),
+				),
+			);
+		},
 
-      return choice(
-        ...table.map(([fn, prec, op]) =>
-          //@ts-ignore
-          fn(
-            prec,
-            seq(
-              field("lhs", $._expr),
-              //@ts-ignore
-              field("operator", alias(op, $.binary_operator)),
-              field("rhs", $._expr),
-            ),
-          ),
-        ),
-      );
-    },
+		number: (_) => {
+			const decimalDigits = /\d+/;
+			const signedInteger = seq(optional("-"), decimalDigits);
+			const exponentPart = seq(choice("e", "E"), signedInteger);
 
-    number: _ => {
-      const decimalDigits = /\d+/;
-      const signedInteger = seq(optional('-'), decimalDigits);
-      const exponentPart = seq(choice('e', 'E'), signedInteger);
+			const decimalIntegerLiteral = seq(
+				optional("-"),
+				choice("0", seq(/[1-9]/, optional(decimalDigits))),
+			);
 
-      const decimalIntegerLiteral = seq(
-        optional('-'),
-        choice(
-          '0',
-          seq(/[1-9]/, optional(decimalDigits)),
-        ),
-      );
+			const decimalLiteral = choice(
+				seq(
+					decimalIntegerLiteral,
+					".",
+					optional(decimalDigits),
+					optional(exponentPart),
+				),
+				seq(decimalIntegerLiteral, optional(exponentPart)),
+			);
 
-      const decimalLiteral = choice(
-        seq(decimalIntegerLiteral, '.', optional(decimalDigits), optional(exponentPart)),
-        seq(decimalIntegerLiteral, optional(exponentPart)),
-      );
+			return token(decimalLiteral);
+		},
 
-      return token(decimalLiteral);
-    },
+		comment: ($) =>
+			seq(
+				"//",
+				optional(
+					field(
+						"marker",
+						choice($.module_doc_comment_marker, $.decl_doc_comment_marker),
+					),
+				),
+				$._comment_body,
+			),
 
-    comment: ($) =>
-      seq(
-        "//",
-        optional(
-          field(
-            "marker",
-            choice($.module_doc_comment_marker, $.decl_doc_comment_marker),
-          ),
-        ),
-        $._comment_body,
-      ),
-
-    module_comments: ($) => repeat1(seq("//!", $._comment_body)),
-    doc_comments: ($) => repeat1(seq("///", $._comment_body)),
-    module_doc_comment_marker: (_) => token.immediate(prec(2, "!")),
-    decl_doc_comment_marker: (_) => token.immediate(prec(2, "/")),
-    _comment_body: (_) => token.immediate(/[^\n]*/),
-  }
-
-  
-
+		module_comments: ($) => repeat1(seq("//!", $._comment_body)),
+		doc_comments: ($) => repeat1(seq("///", $._comment_body)),
+		module_doc_comment_marker: (_) => token.immediate(prec(2, "!")),
+		decl_doc_comment_marker: (_) => token.immediate(prec(2, "/")),
+		_comment_body: (_) => token.immediate(/[^\n]*/),
+	},
 });
 /**
-* Creates a rule to match one or more of the rules separated by a comma
-*
-* @param {RuleOrLiteral} rule
-*
-* @return {SeqRule}
-*
-*/
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @return {SeqRule}
+ *
+ */
 function commaSep1(rule) {
-  return seq(rule, repeat(seq(',', rule)));
+	return seq(rule, repeat(seq(",", rule)));
 }
 
 /**
@@ -262,5 +242,5 @@ function commaSep1(rule) {
  *
  */
 function commaSep(rule) {
-  return optional(commaSep1(rule));
+	return optional(commaSep1(rule));
 }
